575ef8524f4a01c095923897f093641bd461e0bf
Canty <idcanty.2016@mymail.jcsu.edu>
2018-02-01 13:22:05 -0500

    first commit

---
+++ humanlab/Human.java
@@ -0,0 +1,9 @@
+package humanlab;
+
+public class Human {
+
+	public Human() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ humanlab/HumanTest.java
@@ -0,0 +1,10 @@
+package humanlab;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
